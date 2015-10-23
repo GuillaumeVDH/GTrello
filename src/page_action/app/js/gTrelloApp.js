a736cde0335e1502abaf315ab5e60f3c02063359
Callaert Anthony <callaertanthony@gmail.com>
2015-10-23 18:46:23 +0200

    perform to have routes
    basic example with icons

--- src/page_action/app/js/gTrelloApp.js
+++ src/page_action/app/js/gTrelloApp.js
@@ -1,5 +1,16 @@
-var gTrelloApp = angular.module('gTrelloApp', ['ngMaterial']);
+var gTrelloApp = angular.module('gTrelloApp', ['ngMaterial','ngRoute']);
 
-gTrelloApp.config( function ($compileProvider) {
+gTrelloApp.config(function($compileProvider) {
    $compileProvider.aHrefSanitizationWhitelist (/^\s*(chrome-extension):/);
 });
+
+gTrelloApp.config(['$routeProvider', function($routeProvider){
+	var templateFolder = "/src/page_action/app/templates/";
+	$routeProvider
+		.when("/boards", {
+			templateUrl: templateFolder + 'boards.html'
+		})
+		.otherwise({
+			templateUrl: templateFolder + 'test.html'
+		});
+}]);
