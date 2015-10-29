db53aa58b535066b00aad0733281d7e234a04c28
Anthony Callaert <callaertanthony@gmail.com>
2015-10-29 17:28:04 +0100

    restructuration de l'autorisation.

--- src/page_action/app/js/controllers/mainController.js
+++ src/page_action/app/js/controllers/mainController.js
@@ -1,11 +1,63 @@
-gTrelloApp
-	.controller("MainController", ["$scope", "utilService", "$location", function($scope, utilService, $location){
-		utilService.trelloInit();
-		$scope.authorized = Trello.authorized();
+'use strict';
 
-		if(Trello.authorized){
-			$location.url('/boards');
-		} else {
-			$location.url('/test');
-		}
+gTrelloApp
+	.controller("MainController", ["$scope", "utilService", "$location", "$mdDialog", function($scope, utilService, $location, $mdDialog){
+		utilService.trelloInit();
+
+        $scope.login = {
+            isLoggedIn : false,
+            token : null
+        };
+
+        var updateLogin = function(){
+            $scope.login.isLoggedIn = Trello.authorized();
+            $scope.login.token = Trello.token();
+        };
+
+        $scope.login.loginToTrello = function () {
+            Trello.authorize({
+                type: "popup",
+                name: "GTrello",
+                scope: { write: true, read: true },
+                persist: true,
+                success: function () {
+                    Trello.authorize({
+                        interactive: false,
+                        success: function () {
+                            updateLogin();
+                            chrome.tabs.getCurrent(function(tab){
+                                chrome.pageAction.show(tab.id);
+                            });
+                        }
+                    });
+                }
+            })
+        };
+
+		$scope.login.logoutFromTrello = function () {
+            Trello.deauthorize();
+            updateLogin();
+        };
+
+        Trello.authorize({
+            interactive: false,
+            success: updateLogin
+        });
+
+        $scope.showConfirm = function(ev) {
+            // Appending dialog to document.body to cover sidenav in docs app
+            var confirm = $mdDialog.confirm()
+                .title('Please log in to Trello !')
+                .content('You need to authorize GTrello App to access your Trello data')
+                .targetEvent(ev)
+                .parent(angular.element(document.querySelector('md-content')))
+                .ok('Authorize!')
+                .cancel('Cancel');
+            $mdDialog.show(confirm).then(
+                function(){
+                    $scope.login.loginToTrello();
+                },
+                function() {
+            });
+        };
 	}]);
--- src/page_action/app/js/gTrelloApp.js
+++ src/page_action/app/js/gTrelloApp.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var gTrelloApp = angular.module('gTrelloApp', ['ngMaterial','ngRoute']);
 
 gTrelloApp.config(function($compileProvider) {
@@ -7,10 +9,42 @@ gTrelloApp.config(function($compileProvider) {
 gTrelloApp.config(['$routeProvider', function($routeProvider){
 	var templateFolder = "/src/page_action/app/templates/";
 	$routeProvider
+		.when("/", {
+			templateUrl: templateFolder + 'main.html'
+		})
 		.when("/boards", {
 			templateUrl: templateFolder + 'boards.html'
 		})
 		.otherwise({
-			templateUrl: templateFolder + 'test.html'
+			redirectTo: '/'
 		});
 }]);
+
+gTrelloApp
+	.config(['$httpProvider', function($httpProvider) {
+		delete $httpProvider.defaults.headers.common["X-Requested-With"]
+	}]).filter('encodeURIComponent', function() {
+		return window.encodeURIComponent;
+	}).service('TrelloNg', [ '$q', '$rootScope', function($q, $rootScope){
+
+		var TrelloNg = {};
+
+		// wrap Trello queries in angular promise
+		TrelloNg.query = function(entity_name) { // success
+			var deferred = $q.defer();
+			Trello.get(entity_name, function(data) {
+				// wrap call to resolve in $apply as this function is out of the main event loop
+				$rootScope.$apply(function() {
+					deferred.resolve(data);
+				});
+			}, function(response) { // error
+				$rootScope.$apply(function() {
+					deferred.reject(response);
+				});
+			});
+
+			return deferred.promise;
+		};
+
+		return TrelloNg;
+	}]);
